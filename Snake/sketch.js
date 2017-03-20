072ee1b3423a2b1e22f8ea1f0fc9050e7c67d542
ChenX1ao <xiaochenmyzou@gmail.com>
2017-03-20 01:48:11 -0400

    Add files via upload

--- Snake/sketch.js
+++ Snake/sketch.js
@@ -1,5 +1,6 @@
-var scl = 15;
+var scl = 30;
 var gameWidth = 30, gameHeight = 30;
+var gamePause = false;
 var snake, food;
 
 function setup() {
@@ -24,14 +25,20 @@ function keyPressed() {
   }
 
   if (keyCode === 80) {
-    print('Game is paused.')
-    noLoop();
-    print(_loop);
-  } else if (keyCode === 67) {
-    print('Game continues.')
-    loop();
-    print(_loop);
+    if (gamePause === false) {
+      print('Game is paused.')
+      noLoop();
+      gamePause = true;
+    } else {
+      print('Game continues.')
+      loop();
+      gamePause = false;
+    }
   }
+}
+
+function mousePressed() {
+  snake.grow();
 }
 
 function draw() {
@@ -50,23 +57,33 @@ function draw() {
 }
 
 function drawGame() {
+  //draw snake body
+  fill(255);
+  stroke(255);
+  for (var i = 1; i < snake.body.length; i++) {
+    if (snake.body[i].x === snake.body[i - 1].x) {
+      rect((snake.body[i].x + 1/4) * scl, (min(snake.body[i].y, snake.body[i - 1].y) + 1/4) * scl, 2/4 * scl, 6/4 * scl);
+    } else if (snake.body[i].y === snake.body[i - 1].y) {
+      rect((min(snake.body[i].x, snake.body[i - 1].x) + 1/4) * scl, (snake.body[i].y + 1/4) * scl, 6/4 * scl, 2/4 * scl);
+    }
+  }
   //draw snake head
   fill(255);
   stroke(255);
-  rect(snake.body[0].x * scl, snake.body[0].y * scl, scl, scl);
-  //draw snake body
-  fill(245);
-  stroke(245);
-  for (var i = 1; i < snake.body.length; i++) {
-    rect(snake.body[i].x * scl, snake.body[i].y * scl, scl, scl);
-  }
+  rect((snake.body[0].x + 1/6) * scl, (snake.body[0].y + 1/6) * scl, 4/6 * scl, 4/6 * scl);
+
   //draw Food
   fill(255, 0, 100);
   stroke(255, 0, 100);
-  rect(food.pos.x * scl, food.pos.y * scl, scl, scl);
+  rect((food.pos.x + 1/6) * scl, (food.pos.y + 1/6) * scl, 4/6 * scl, 4/6 * scl);
+}
+
+function availableLocations() {
+  // array to store available locations
 }
 
 function pickLocation() {
+  // random from available locations
   return createVector(floor(random(gameWidth)), floor(random(gameHeight)));
 }
 
